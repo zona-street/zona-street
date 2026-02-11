1e4f8464e64baddc02c691fdb3d04c5fecc22b5d
SCamposs <soarescmpos@gmail.com>
2026-02-11 17:12:19 -0300

    Update drizzle.config.ts

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,17 +1,16 @@
-import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL não está definida no arquivo .env");
 }
 
-export default defineConfig({
+export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
   },
   verbose: true,
   strict: true,
-});
+};
